599f17cf2161cee4cee68833d40d831c880e5f9a
Emmanuel Gomez <gomezgtzemmanuel@gmail.com>
2022-08-24 01:11:41 -0500

    React course
    React course introduction

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// //<p> hola mundo </p>
+// const Li = ({ children, estado, casa, edad }) => {
+//   console.log(casa, edad);
+//   return (
+//     <li>
+//       {children} esta {estado}
+//     </li>
+//   );
+// };
+// const X = () => (
+//   <ul>
+//     <Li
+//     estado={"feliz"}
+//     casa={false}
+//     edad={29}>
+//       Chanchito
+//     </Li>
+//     <Li estado={"triste"}>Chanchito</Li>
+//     <Li estado={"emocionado"}>Emmanuel</Li>
+//   </ul>
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<X />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
